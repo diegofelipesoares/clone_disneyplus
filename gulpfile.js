e4d1158c2d2d5af8dbbfe4cd0ab0a52c7e466d21
“Diego <diegofelipesoares@gmail.com>
2024-09-14 15:59:05 -0300

    construindo o hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
+//Importações dos plugis do GULP
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
+//Funções de configuração dos plugins
 function styles(){
     return gulp.src('./src/styles/*.scss')
     .pipe(sass({outputStyle:'compressed'}))
     .pipe(gulp.dest('dist/css'));
 }
 
-exports.default = styles;
+// function images(){
+//     return gulp.src('./src/images/**/*') //2 asteriscos é para acessar todas as pastas e /* pegar todas extensões
+//     .pipe(imagemin())
+//     .pipe(gulp.dest('./dist/images'));
+// }
+
+//Exports das funções
+exports.default = gulp.parallel(styles);
 
 //função de observação
 exports.watch = function(){
